05e3766603ac9f6ea67f9dea70c3c252ba8e4c4c
gcaddick <gcaddick@softwareinstitute.com>
2021-11-22 09:45:07 -0500

    Restricted acls, policies for S3 buckets. Successfull terraform apply.

--- main.tf
+++ main.tf
@@ -90,3 +90,21 @@ resource "aws_s3_bucket" "LogsFromCloudTrail" {
     }
  }
 }
+
+resource "aws_s3_bucket_public_access_block" "LogsFromCloudTrail-ACCESS" {
+  bucket = aws_s3_bucket.LogsFromCloudTrail.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_public_access_block" "LogAccessFromLogBucket-ACCESS" {
+  bucket = aws_s3_bucket.LogAccessFromLogBucket.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
