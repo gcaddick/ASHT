4d7c753c60b4d5561d23d5e838affe1e2c13f7e4
gcaddick <gcaddick@softwareinstitute.com>
2021-11-23 08:08:04 -0500

    Added what I think is the correct pattern for metric filter

--- main.tf
+++ main.tf
@@ -216,11 +216,6 @@ resource "aws_iam_role_policy" "CloudWatchLogRolePolicy" {
 POLICY
 }
 
-
-
-
-
-
 resource "aws_cloudtrail" "EnableAllRegionCT" {
     name = "Account-CloudTrail"
     s3_bucket_name = "${aws_s3_bucket.LogsFromCloudTrail.id}"
@@ -250,3 +245,37 @@ resource "aws_cloudwatch_log_group" "CloudTrailLogGroup" {
     name = "cloudtrail-log-group"
 }
 
+resource "aws_cloudwatch_log_metric_filter" "UnAuthAPI" {
+    name = "unauth-api"
+    pattern = "\"$.errorCode = AccessDenied\""
+
+    log_group_name = "${aws_cloudwatch_log_group.CloudTrailLogGroup.id}"
+    metric_transformation {
+        name = "UnAuthAPICalls"
+        namespace = "CloudTrailMetrics"
+        value = 1
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "AlarmForUnAthorizedAPIcall" {
+    alarm_name = "alarm-for-unathorized-api-call"
+    comparison_operator = "GreaterThanThreshold"
+    evaluation_periods = "1"
+    metric_name = "UnAuthAPICalls"
+    namespace = "CloudTrailMetrics"
+    period = "60"
+    threshold = "0"
+    statistic = "SampleCount"
+    alarm_actions = [aws_sns_topic.UnauthorizedAPI.arn]
+}
+
+
+resource "aws_sns_topic" "UnauthorizedAPI" {
+    name = "UnauthorizedAPICall"
+}
+
+resource "aws_sns_topic_subscription" "UnAuthAPIemail" {
+    topic_arn = "${aws_sns_topic.UnauthorizedAPI.arn}"
+    protocol = "email"
+    endpoint = "ledore5458@kyrescu.com"
+}
